bdc3a93b28db574d8160851af2fc6863064e1ee6
jfischlmayr <jan.fischlmayr@gmx.net>
2021-05-05 21:27:12 +0200

    data context

---
+++ CashRegister/CashRegister.Shared/Model.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CashRegister.Shared
+{
+    public class Product
+    {
+        [JsonPropertyName("id")]
+        public int ID { get; set; }
+
+        [JsonPropertyName("productName")]
+        public string ProductName { get; set; } = string.Empty;
+
+        [JsonPropertyName("unitPrice")]
+    public decimal UnitPrice { get; set; }
+    }
+
+    public class ReceiptLine
+    {
+        [JsonPropertyName("id")]
+        public int ID { get; set; }
+
+        [JsonPropertyName("product")]
+        public Product Product { get; set; }
+
+        [JsonPropertyName("amount")]
+        public int Amount { get; set; }
+
+        [JsonPropertyName("totalPrice")]
+        public decimal TotalPrice { get; set; }
+    }
+
+    public class Receipt
+    {
+        [JsonPropertyName("id")]
+        public int ID { get; set; }
+
+        [JsonPropertyName("receiptTimestamp")]
+        public DateTime ReceiptTimestamp { get; set; }
+
+        [JsonPropertyName("receiptLines")]
+        public List<ReceiptLine> ReceiptLines { get; set; } = new();
+
+        [JsonPropertyName("totalPrice")]
+        public decimal TotalPrice { get; set; }
+    }
+}
---
+++ CashRegister/CashRegister.WebAPI/CashRegisterDataContext.cs
@@ -0,0 +1,32 @@
+﻿using CashRegister.Shared;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CashRegister.WebAPI
+{
+    public class CashRegisterDataContext : DbContext
+    {
+        public CashRegisterDataContext(DbContextOptions<CashRegisterDataContext> options)
+        : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().Property(p => p.ProductName).IsRequired();
+            modelBuilder.Entity<Product>().Property(p => p.UnitPrice).IsRequired().HasColumnType("numeric(6,2)");
+
+            modelBuilder.Entity<ReceiptLine>().Property(rl => rl.Amount).IsRequired();
+            modelBuilder.Entity<ReceiptLine>().Property(rl => rl.TotalPrice).IsRequired().HasColumnType("numeric(6,2)");
+
+            modelBuilder.Entity<Receipt>().Property(r => r.ReceiptTimestamp).IsRequired();
+            modelBuilder.Entity<Receipt>().Property(r => r.TotalPrice).IsRequired().HasColumnType("numeric(6,2)");
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Receipt> Receipts { get; set; }
+        public DbSet<ReceiptLine> ReceiptLines { get; set; }
+    }
+}
