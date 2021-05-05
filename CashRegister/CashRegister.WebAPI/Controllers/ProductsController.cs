eb5a2f2374ba66264cee074517a6795a75ea411b
jfischlmayr <jan.fischlmayr@gmx.net>
2021-05-05 21:50:11 +0200

    ProductsController

---
+++ CashRegister/CashRegister.WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using CashRegister.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CashRegister.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly CashRegisterDataContext _context;
+
+        public ProductsController(CashRegisterDataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Product>> Get([FromQuery] string? nameFilter = null)
+        {
+            IQueryable<Product> products = _context.Products;
+
+            if (!string.IsNullOrEmpty(nameFilter))
+            {
+                products = products.Where(p => p.ProductName.Contains(nameFilter));
+            }
+
+            return await products.ToListAsync();
+        }
+    }
+}
